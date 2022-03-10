5fffa3e5f4c779e6ae1ae0d493adda3229eb6e8a
Klittina <krisztike113@gmail.com>
2022-03-10 15:05:19 +0100

    kivonás

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -11,8 +11,30 @@ public class CsoportMunkaSzamolas {
     public static void osztas() {
         
     }
+    public static int  kivonas(){
+        int a;
+        int b;
+        int c = 2;
+        int valasz=0;
+        int eredmeny = 0;
+        while(c<1 && c>100){
+            a = rnd.nextInt(100)+1;
+            b = rnd.nextInt(100)+1;
+            c = a-b;
+            if(c >=1 && c<=100){
+                System.out.printf("Írd le a(z) %d - %d eredményét!", a, b);
+                valasz = bekertszam.nextInt();
+            }
+            if(c == valasz){
+                eredmeny = 1;
+            }
+            
+        }
+        return eredmeny;
+    }
 
     public static void main(String[] args) {
+        kivonas();
     }
     
 }
