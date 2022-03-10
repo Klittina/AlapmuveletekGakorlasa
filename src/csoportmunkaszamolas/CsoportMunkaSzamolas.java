51a1c9770aadf5c16aa2640f6790d1c0e3894304
Xu Jiyu <matexu97@gmail.com>
2022-03-10 16:05:36 +0100

    befejezett projekt

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -9,23 +9,27 @@ public class CsoportMunkaSzamolas {
     static Scanner bekertszam = new Scanner(System.in);
     static Random rnd = new Random();
 
-        public static int osszead(){
-        int a,b,c;
+    public static int osszead() {
+        int a, b, c;
         int helyes;
-        
-        a = rnd.nextInt(100)+1;
-        b = rnd.nextInt(100)+1;
-        while(a+b>=100){
-              a = rnd.nextInt(100)+1;
-              b = rnd.nextInt(100)+1;
+
+        a = rnd.nextInt(100) + 1;
+        b = rnd.nextInt(100) + 1;
+        while (a + b >= 100) {
+            a = rnd.nextInt(100) + 1;
+            b = rnd.nextInt(100) + 1;
         }
-        c = a+b;
-        System.out.printf("%d+%d=",a,b);
-        int valasz=bekertszam.nextInt();
-        if (valasz==c) helyes=1;
-        else helyes=0;
+        c = a + b;
+        System.out.printf("%d+%d=", a, b);
+        int valasz = bekertszam.nextInt();
+        if (valasz == c) {
+            helyes = 1;
+        } else {
+            helyes = 0;
+        }
         return helyes;
-}
+    }
+
     public static int osztas() {
         int a, b, c;
         int valasz;
@@ -53,7 +57,7 @@ public class CsoportMunkaSzamolas {
         int c = 2;
         int valasz = 0;
         int eredmeny = 0;
-        while (c < 1 && c > 100) {
+        while (c > 1 && c < 100) {
             a = rnd.nextInt(100) + 1;
             b = rnd.nextInt(100) + 1;
             c = a - b;
@@ -70,25 +74,41 @@ public class CsoportMunkaSzamolas {
     }
 
     public static void main(String[] args) {
-        System.out.println("Milyen műveletet szeretne gyakorolni?");
-        String gyakorlojel = jel.nextLine();
-        int helyes = 0;
-        int i = 0;
-        while(i < 10){
-        while (!(gyakorlojel.equals("+")) || !((gyakorlojel.equals("-"))) || !((gyakorlojel.toLowerCase().equals("x"))) || (gyakorlojel.equals("/"))) {
-            System.out.println("A jel nem felel meg + , - , x vagy / jelt adja meg. Milyen műveletet szeretne gyakorolni?");
-            gyakorlojel = jel.nextLine();
-        }
-        if (gyakorlojel.equals("+")) {
-            helyes += osszead();
-        } else if (gyakorlojel.equals("-")) {
-            helyes += kivonas();
-        } else if (gyakorlojel.toLowerCase().equals("x")) {
-            helyes += szorzas();
-        } else if (gyakorlojel.equals("/")) {
-            helyes += osztas();
-        }
-        i++;
+        Boolean ujjatek = true;
+        while (ujjatek) {
+            System.out.println("Milyen műveletet szeretne gyakorolni?");
+            String gyakorlojel = jel.nextLine();
+            int helyes = 0;
+            int i = 0;
+            while (i < 10) {
+                while (!(gyakorlojel.equals("+") || gyakorlojel.equals("-") || gyakorlojel.toLowerCase().equals("x") || gyakorlojel.equals("/"))) {
+                    System.out.println("A jel nem felel meg + , - , x vagy / jelt adja meg. Milyen műveletet szeretne gyakorolni?");
+                    gyakorlojel = jel.nextLine();
+                }
+                if (gyakorlojel.equals("+")) {
+                    helyes += osszead();
+                } else if (gyakorlojel.equals("-")) {
+                    helyes += kivonas();
+                } else if (gyakorlojel.toLowerCase().equals("x")) {
+                    helyes += szorzas();
+                } else if (gyakorlojel.equals("/")) {
+                    helyes += osztas();
+                }
+                i++;
+            }
+            int joszazalek = (helyes / 10) * 100;
+            System.out.println("Ennyi lett jó:" + helyes + "/10" + " és ennyi százalékod lett: " + joszazalek+"%.");
+            System.out.println("Akarsz új játékot?: ");
+            String valasz = jel.nextLine();
+            while (!(valasz.toLowerCase().equals("nem") || valasz.toLowerCase().equals("igen"))) {
+                System.out.println("A válasz nem jó. Akarsz új játékot?: ");
+                valasz = jel.nextLine();
+            }
+            if (valasz.toLowerCase().equals("igen")) {
+                ujjatek = true;
+            } else {
+                ujjatek = false;
+            }
         }
     }
 
