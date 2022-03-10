ca80d63d536826f3f1db5edd7bc1fa07f6aae556
P-Andris <pillerandras0714@gmail.com>
2022-03-10 14:41:52 +0100

    java

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class CsoportMunkaSzamolas {
     static Scanner jel = new Scanner(System.in);
     static Scanner bekertszam = new Scanner(System.in);
+
+    public static void osztas() {
+        
+    }
+
     public static void main(String[] args) {
         
     }
