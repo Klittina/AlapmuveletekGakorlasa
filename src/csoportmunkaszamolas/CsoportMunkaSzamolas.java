75359fd4b18d96dba56ee23eae7f0588c9db030d
P-Andris <pillerandras0714@gmail.com>
2022-03-10 15:03:02 +0100

    Osztás (kész)

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -8,11 +8,26 @@ public class CsoportMunkaSzamolas {
     static Scanner bekertszam = new Scanner(System.in);
     static Random rnd = new Random();
 
-    public static void osztas() {
-        
+    public static int osztas() {
+        int a, b, c;
+        int valasz;
+        int helyes;
+
+        a = rnd.nextInt(100) + 1;
+        b = rnd.nextInt(a) + 1;
+        while(a % b != 0) {
+            b = rnd.nextInt(a) + 1;
+        }
+        c = a / b;
+        System.out.printf("\n%d / %d = ", a, b);
+        valasz = bekertszam.nextInt();
+        if(valasz == c) helyes = 1;
+        else helyes = 0;
+        return helyes;
     }
 
     public static void main(String[] args) {
+        osztas();
     }
     
 }
