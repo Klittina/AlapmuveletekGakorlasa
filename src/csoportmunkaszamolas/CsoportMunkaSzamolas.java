4f80714d4a07b4e3eae86f19b8df69fc33059bb8
P-Andris <pillerandras0714@gmail.com>
2022-03-10 14:42:45 +0100

    Random

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class CsoportMunkaSzamolas {
     static Scanner jel = new Scanner(System.in);
     static Scanner bekertszam = new Scanner(System.in);
+    static Random rnd = new Random();
 
     public static void osztas() {
         
