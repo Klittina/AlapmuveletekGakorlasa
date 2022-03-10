94f914a3b34fe4a2fc9ec68eda769db94ee21500
panel05 <pantlbalazs@gmail.com>
2022-03-10 14:58:42 +0100

    szorzás

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -13,6 +13,24 @@ public class CsoportMunkaSzamolas {
     }
 
     public static void main(String[] args) {
+        szorzas();
+    }
+
+    public static int szorzas() {
+        int a = rnd.nextInt(10)+1;
+        int b = rnd.nextInt(10)+1;
+        int c = a*b;
+        int helyes;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Mi a megoldás ennél a szorzásnál: %d * %d", a, b);
+        int valasz = bekertszam.nextInt();
+        System.out.printf("A szorzás eredménye: %d\n",c);
+        if(valasz == c){
+            helyes = 1;
+        }else{
+            helyes = 0;
+        }
+        return helyes;
     }
     
 }
