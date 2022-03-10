5afd8f74b0b2a7740ac1ed4e74c1cdc2f099b20c
Xu Jiyu <matexu97@gmail.com>
2022-03-10 14:41:30 +0100

    Random

--- src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
+++ src/csoportmunkaszamolas/CsoportMunkaSzamolas.java
@@ -1,12 +1,12 @@
 package csoportmunkaszamolas;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class CsoportMunkaSzamolas {
     static Scanner jel = new Scanner(System.in);
     static Scanner bekertszam = new Scanner(System.in);
     public static void main(String[] args) {
-        
     }
     
 }
